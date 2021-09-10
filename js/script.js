1e1ef3f19d940f3d99189142009a421bd0c1d2cf
A <bilaukraina@gmail.com>
2021-09-10 13:52:55 +0300

    first script code

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последнх просмотренных фильмов', ''),
+      b = +prompt('на сколько оцените его', ''),
+      c = prompt('один из последнх просмотренных фильмов', ''),
+      d = +prompt('на сколько оцените его', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
